efd54bd1ffc680c03758f5dedc549ea9a0be3b9a
sam856 <76130128+sam856@users.noreply.github.com>
2023-03-29 01:11:50 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Sidebar from "./Component/SharedComponent/Sidebar/Sidebar/Sidebar/Sidebar";
+import Navbar from "./Component/SharedComponent/Sidebar/Sidebar/Sidebar/TopNav/Navbar";
+
+import './Style/App.css'
+
+
+
+
+function App() {
+  return (
+    <>
+     <Navbar />
+        <Sidebar />
+        
+          
+      
+    
+    </>
+  );
+}
+
+export default App;
