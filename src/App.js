f8a315652a5013a516a75c2c5eec3eaffabe6241
sam856 <76130128+sam856@users.noreply.github.com>
2023-03-30 15:51:52 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import Sidebar from './Component/SharedComponent/Sidebar/Sidebar';
+import Navbar from './Component/SharedComponent/TopNav/Navbar';
+import AssignCourse from './Pages/AssignCourse';
+import './Style/App.css'
+
+
+
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <AssignCourse />
+     <Sidebar />
+        
+          
+      
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/AssignCourse.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../Style/AssignCourse.css'
+
+const AssignCourse = () => {
+    return (
+      <div className='TableContaier'>
+        <table >
+            <thead>
+                <tr>
+                    <th>Course name</th>
+                    <th>Instractor</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+
+                <tr>
+                    <td>
+
+                    </td>
+                          <td>
+                            <label>
+                                <select>
+
+                                    <option value="fruit"></option>
+
+                                    <option value="vegetable"></option>
+
+                                    <option value="meat"></option>
+
+                                </select>
+
+                            </label>
+
+                          </td>
+
+                </tr>
+
+                }
+            </tbody>
+        </table>
+      </div>
+    )
+}
+
+export default AssignCourse
